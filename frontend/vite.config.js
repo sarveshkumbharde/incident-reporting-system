6fd4a1b3fd4e27abbe6b8b382bee332a7c51478a
Sarvesh Kumbharde <sarveshkumbharde@gmail.com>
2025-10-25 12:45:20 +0530

    updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    '/api':{
-      target: 'http://localhost:5000',  
-      changeOrigin: true,
-      secure: false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
