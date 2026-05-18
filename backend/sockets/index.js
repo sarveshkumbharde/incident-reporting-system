f037e9ac6327a6205b1d1cab5ae030f3506e17f8
Sarvesh Kumbharde <sarveshkumbharde@gmail.com>
2026-05-18 17:59:28 +0530

    asf

--- backend/sockets/index.js
+++ backend/sockets/index.js
@@ -1,26 +1,11 @@
-const onlineUsers = new Map();
-
 exports.socketHandler = (io) => {
 
   io.on("connection", (socket) => {
 
     socket.on("register", ({ userId, role }, ack) => {
-      onlineUsers.set(userId.toString(), socket.id);
       socket.join(userId.toString()); 
-      console.log("🧠 onlineUsers:", [...onlineUsers.entries()]);
-
+      console.log(`🧠 User ${userId} joined room ${userId}`);
     });
 
-    socket.on("disconnect", () => {
-      for (let [userId, sockId] of onlineUsers) {
-        if (sockId === socket.id) {
-          onlineUsers.delete(userId);
-          break;
-        }
-      }
-    });
   });
 };
-
-
-exports.getUserSocket = (userId) => onlineUsers.get(userId.toString());
